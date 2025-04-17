b9221c51ef62833ac44048f63195ff0be7472f65
Kishan Rajendra <86339947+kishan20-00@users.noreply.github.com>
2025-04-17 19:14:22 +0530

    Updated code

---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// var firstName = "John";
+
+// console.log(firstName);
+
+// ("John");
+
+// console.log(age);
+// const age =26;
+// age = 27;
+// console.log(age);
+// message = "hello world";
+
+// var message = "hello again";
+// console.log(message);
+
+
+const name = "Reed";
+console.log(name);
+
+let count = 100;
+count = 101;
+count = 101;
+
+console.log(count);
+
+const originalPrice = 50;
+const percentOff = 20;
+const salePrice = originalPrice * (percentOff/100);
+
+console.log(salePrice);
+
+var originalPrice1 = 50;
+var percentOff1 = 20;
+var salePrice1 = originalPrice1 * (percentOff / 100);
+
+console.log(salePrice1);
+
+
+var price = 20;
+var isSale = true;
+
+if (isSale) {
+    //discount the price of the product
+    var price = 20 - 2;
+
+    console.log('Sale price', price);
+}
+
+console.log('price', price)
+
+
+var price = 20;
+var isSale = true;
+
+if (isSale) {
+    //discount the price of the product
+    const price = 20 - 2;
+
+    console.log('Sale price', price);
+}
+
+console.log('price', price)
+
+
